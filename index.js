2a5ac802e66b14fd7509f193aac483773cd98f42
Mirabbosbek9778 <jwicj969@gmail.com>
2023-02-25 20:20:53 +0500

    Masalalar javobi forloop, switch case

---
+++ index.js
@@ -0,0 +1,190 @@
+// let num = Math.round(Math.random() * 1000);
+// console.log(num);
+
+// e = parseInt(num / 100); //3 xonalik
+// num = num % 100;
+
+// d = parseInt(num / 10); // 2 xonalik
+// num = num % 10;
+// c = num; ///birlik honasi
+// let a = "",
+//   b = "",
+//   f = "";
+
+// switch (e) {
+//   case 1:
+//     a += "bir yuz";
+//     break;
+//   case 2:
+//     a += "ikki yuz";
+//     break;
+//   case 3:
+//     a += "uch yuz";
+//     break;
+//   case 4:
+//     a += "to'rt yuz";
+//     break;
+//   case 5:
+//     a += "besh yuz";
+//     break;
+//   case 6:
+//     a += "olti yuz";
+//     break;
+//   case 7:
+//     a += "yetti yuz";
+//     break;
+//   case 8:
+//     a += "sakkiz yuz";
+//     break;
+//   case 9:
+//     a += "toqqiz yuz";
+//     break;
+// }
+// switch (d) {
+//   case 1:
+//     b += "o'n";
+//     break;
+//   case 2:
+//     b += "yigirma";
+//     break;
+//   case 3:
+//     b += "o'ttiz";
+//     break;
+//   case 4:
+//     b += "qirq";
+//     break;
+//   case 5:
+//     b += "ellik";
+//     break;
+//   case 6:
+//     b += "oltimish";
+//     break;
+//   case 7:
+//     b += "yettimish";
+//     break;
+//   case 8:
+//     b += "sakkson";
+//     break;
+//   case 9:
+//     b += "toqqson";
+//     break;
+// }
+// switch (c) {
+//   case 1:
+//     f += "bir";
+//     break;
+//   case 2:
+//     f += "ikki";
+//     break;
+//   case 3:
+//     f += " uch";
+//     break;
+//   case 4:
+//     f += " tort";
+//     break;
+//   case 5:
+//     f += "besh";
+//     break;
+//   case 6:
+//     f += "olti";
+//     break;
+//   case 7:
+//     f += "yetti";
+//     break;
+//   case 8:
+//     f += "sakkiz";
+//     break;
+//   case 9:
+//     f += "toqqiz";
+//     break;
+// }
+// console.log(a, b, f);
+
+///////////////////////////////////
+// let num = 100,
+//   sum = 0;
+
+// for (let j = 2; j <= num; j++) {
+//   for (let i = 1; i <= j; i++) {
+//     if (j % i === 0) {
+//       sum++;
+//     }
+//   }
+//   if (sum === 2) {
+//     console.log(`tub son : ${j}`);
+//   } // else {
+//   // console.log(`tub son emas : ${j}`);
+//   //   }
+//   sum = 0;
+// }
+//
+
+///////////////////////////////
+
+// let browser='Edge';
+
+// switch (browser) {
+//   case "Edge":
+//     console.log("You have got the Edge!");
+//     break;
+//   case "chrome":
+//   case "firefox":
+//   case "safari":
+//   case "opera":
+//     console.log("okay we support these browsers too");
+//     break;
+//   default:
+//     console.log("we hope that this page looks ok!");
+// }
+/////////////////////////////
+
+// let a = 10;
+// let b = 0;
+// let c = 0;
+
+// for (let i = 1; i <= a; i++) {
+//   if (a % i != 0) {
+//     b += i;
+//     c++;
+//   }
+// }
+// console.log(parseFloat(b / c));
+
+///////////////////////////////////////////////////////
+
+// for (let i = 0; i < 3; i++) {
+//   console.log(`number ${i}!`); /// number 0!  number 1! number 2!
+// }
+
+/////////////////////////////////
+
+// let i = 1;
+// for (i = 1; i <=5; i++) {
+//   if (i % 2 == 0) {
+//     console.log(`juft  son : ${i}`);
+//   }else{
+//     console.log(`toq son : ${i}`);
+//   }
+// }
+///////////////////////////////
+
+// let a = Math.round(Math.random() * 5);
+// let a=5;
+// console.log(a);
+// // let a=3
+// // let str=""
+// // console.log('a \nb');
+
+// for (let i = 1; i <= a; i++) {
+//   for (let j = 1; j <= a-i; j++) {
+//     console.log("  ", 2);
+//   }
+//   for (let j = 1; j <= i; j++) {
+//     console.log("*   ");
+//   }
+//   console.log("\n ");
+// }
+
+// //     *
+// //   *   *
+// // *   *   * 3y 2b
